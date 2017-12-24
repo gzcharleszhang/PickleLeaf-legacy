9811df5958aeecf29671aa66f04fe17c3d6636a7
Charles Zhang <gzcharleszhang@hotmail.com>
2017-12-24 13:01:23 -0500

    Added Book Page
    - Displays a Book's detailed information by accessing /book/:bookid

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var Book = require('../models/book');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'UW Textbooks' });
 });
 
-/* GET submitted page */
-router.get('/submitted/:bookid', function(req, res, next){
-  res.render('submitted', {
-    title: 'UW Textbooks', book_id: req.params.bookid
-  })
+/* GET book page */
+router.get('/book/:bookid', function(req, res, next){
+  var bookid =req.params.bookid;
+  var book = new Book();
+
+  Book.findOne({ '_id': bookid }, 'title author price course description username', function (err, book) {
+    if (err) return handleError(err);
+    res.render('book', {
+      title: 'UW Textbooks',
+        book_id: bookid,
+        book_title: book.title,
+        author: book.author,
+        course: book.course,
+        price: book.price,
+        description: book.description,
+        seller: book.username
+    })
+  });
 });
 
 
