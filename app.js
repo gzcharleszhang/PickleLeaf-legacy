2f85361b2c5d540e563e907506b2260e366f8c20
Charles Zhang <gzcharleszhang@hotmail.com>
2018-01-18 18:46:44 -0500

    Added app.js

---
+++ app.js
@@ -0,0 +1,83 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://uwtextbooks:caliorbust2018@ds039155.mlab.com:39155/uwtextbooks", {
+      useMongoClient: true
+});
+var db = mongoose.connection;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
+var flash = require('connect-flash');
+var index = require('./routes/index');
+var submit = require('./routes/submit');
+var users = require('./routes/users');
+var sell = require('./routes/sell');
+var messages = require('./routes/messages');
+var app = express();
+
+// MongoDB
+var mongo = require('mongodb');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(expressSession({secret: 'Conrad', saveUninitialized: false, resave: false}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash Middleware
+app.use(flash());
+
+// Global Vars
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Routes
+app.use('/', index);
+app.use('/submit', submit);
+app.use('/users', users);
+app.use('/sell', sell);
+app.use('/messages', messages);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', {
+        title: "Error"
+    });
+});
+
+module.exports = app;
